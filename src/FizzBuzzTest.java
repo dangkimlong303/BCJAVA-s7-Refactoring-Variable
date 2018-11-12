8ff6d066abab49748fef5d117736e6186dca6b4c
dangkimlong303 <dangkimlong303@gmail.com>
2018-11-12 11:46:40 +0700

    refactoring

---
+++ src/FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        System.out.println(FizzBuzz.isFizzBuzz(15));
+        System.out.println(FizzBuzz.isFizzBuzz(9));
+        System.out.println(FizzBuzz.isFizzBuzz(10));
+        System.out.println(FizzBuzz.isFizzBuzz(13));
+    }
+}
